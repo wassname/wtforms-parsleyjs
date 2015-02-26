2eb992921633db9efc9947e510a25672eb8c6259
James O'Toole <jamesotoole1996@hotmail.co.uk>
2015-02-26 01:24:26 +0000

    Fixed checking is in array incorrectly

--- scripts/parsley-anyof.js
+++ scripts/parsley-anyof.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
     window.ParsleyValidator.addValidator('anyof', function (value, array) {
-        return array.indexOf(value);
+        return array.indexOf(value) >= 0;
     }, 32).addMessage('en', 'anyof', 'The value you have given is not a listed option.')
 });
