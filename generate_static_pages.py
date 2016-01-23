6d39448df74ed46cc0d80578ffe661664dda9718
Is Isilon <wassname@wassname.org>
2016-01-24 06:52:45 +0800

    Added script to generate index.html for gh-pages

---
+++ generate_static_pages.py
@@ -0,0 +1,13 @@
+from wtformsparsleyjs.sample.sample import app
+from flask_frozen import Freezer
+import shutil
+from os.path import join
+freezer = Freezer(app)
+
+if __name__ == '__main__':
+
+    # rebuild static files using flask_frozen
+    freezer.freeze()
+
+    # copy result to index.html
+    shutil.copy( join('wtformsparsleyjs', 'sample', 'build', 'parsley_testform'), 'index.html')
